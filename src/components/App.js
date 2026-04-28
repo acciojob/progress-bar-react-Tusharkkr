61f30a73dac5cb2da7f7b971c386bdd3e0cef14e
Tushar kumar <narutoboi41@gmail.com>
2026-04-28 13:51:24 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [inc, setInc] = useState(0)
+  let [obj, setObj] = useState({ width: '0%' })
+
+  function increment() {
+    setObj({ ...obj, width: `${inc}%` })
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (inc < 100) {
+        setInc(+inc + +1)
+        increment()
+      }
+    }, 200)
+  }, [inc])
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Progress Bar</h1>
+      <div className="container">
+        <div className="inner" style={obj}></div>
+      </div>
+      <p>{inc}%</p>
     </div>
   )
 }
